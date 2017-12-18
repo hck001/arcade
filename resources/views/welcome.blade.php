45e6beb1182c23f09b4364d03f99748cd6c2b96e
Hasan Can Katipoğlu <hasancan@Hasan-MacBook-Pro.local>
2017-12-18 15:58:29 +0400

    viewlerde güncelleme

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,7 @@
 <html>
 <head>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script type="text/javascript" src="{{ asset('js/scripts.js') }}"></script>
     arcade system hck 001
 </head>
 <body>
